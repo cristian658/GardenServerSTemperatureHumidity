d54f2364ba66404d341b556ad3dbfadfcc239865
Cristian Quezada <cristian.quezada@subdere.gov.cl>
2016-03-31 10:01:09 -0300

    Base

---
+++ src/main/java/agro/massacre/garden/server/sensor/dth22/GardenServerSTemperatureHumidityApplication.java
@@ -0,0 +1,12 @@
+package agro.massacre.garden.server.sensor.dth22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenServerSTemperatureHumidityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenServerSTemperatureHumidityApplication.class, args);
+	}
+}
---
+++ src/test/java/agro/massacre/garden/server/sensor/dth22/GardenServerSTemperatureHumidityApplicationTests.java
@@ -0,0 +1,16 @@
+package agro.massacre.garden.server.sensor.dth22;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GardenServerSTemperatureHumidityApplication.class)
+public class GardenServerSTemperatureHumidityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
